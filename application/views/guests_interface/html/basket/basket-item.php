e6b35daf6a26e2268870d9af961bd177e4928bd0
KostyaD <thedamaxstudio@gmail.com>
2013-11-07 13:13:17 +0400

    Basket and css

--- application/views/guests_interface/html/basket/basket-item.php
+++ application/views/guests_interface/html/basket/basket-item.php
@@ -1,6 +1,6 @@
 <div class="basket-item basket-book-item" data-book-id="<?=$book['id'];?>">
 	<a class="basket-rm remove-book-in-basket no-clickable" href=""></a>
-	<div class="basket-img"><img src="<?=baseURL($book['thumbnail']);?>"></div>
+	<div class="basket-img">&nbsp;<img src="<?=baseURL($book['thumbnail']);?>"></div>
 	<a href="<?=site_url($this->uri->language_string.'/'.$book['page_address'])?>" class="basket-item-name"><?=$book[$this->uri->language_string.'_title'];?></a>
 	<p class="basket-price">
 	<?php if($book['price_action'] > 0):?>
