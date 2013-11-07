e1f1db829bfbe636837f1fba208382f64608ae36
KostyaD <thedamaxstudio@gmail.com>
2013-11-07 17:20:35 +0400

    some basket fixes

--- application/views/guests_interface/html/basket/basket-item-sale-full.php
+++ application/views/guests_interface/html/basket/basket-item-sale-full.php
@@ -3,11 +3,11 @@
 		<a class="basket-rm remove-book-in-basket book-as-action no-clickable" href="#"></a>
 		<div class="basket-img">&nbsp;<img src="<?=baseURL($book['thumbnail']);?>"></div>
 		<a href="<?=site_url($this->uri->language_string.'/'.$book['page_address'])?>" class="basket-item-name"><?=$book[$this->uri->language_string.'_title'];?></a>
-		<p class="author">
+		<div class="basket-author">
 			<?php for($j=0;$j<count($book['authors']);$j++):?>
 				<a href="<?=site_url('catalog?author='.$book['authors'][$j]['id'])?>"><?=$book['authors'][$j][$this->uri->language_string.'_name'];?></a><?php if(isset($book['authors'][$j+1])):?>,<br/> <?php endif;?>
 			<?php endfor;?>
-		</p>
+		</div>
 	</div>
 	<div class="basket-sale-bottom"></div>
 </div>
