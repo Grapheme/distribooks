a6952a61534b897e3d93a89fff5faa832e4d42fa
KostyaD <thedamaxstudio@gmail.com>
2013-11-08 21:50:15 +0400

    sooome

--- application/views/guests_interface/html/book-in-shop.php
+++ application/views/guests_interface/html/book-in-shop.php
@@ -10,13 +10,15 @@
 			<a href="<?=site_url('catalog?author='.$book['authors'][$j]['id'])?>"><?=$book['authors'][$j][$this->uri->language_string.'_name'];?></a><?php if(isset($book['authors'][$j+1])):?>,<br/> <?php endif;?>
 		<?php endfor;?>
 		</p>
-		<div class="rating-shop">
-			<img src="<?=baseURL('img/star.png');?>">
-			<img src="<?=baseURL('img/star.png');?>">
-			<img src="<?=baseURL('img/star.png');?>">
-			<img src="<?=baseURL('img/star-none.png');?>">
-			<img src="<?=baseURL('img/star-none.png');?>">
-		</div>
+		<div class="input select rating-f">
+            <select class="example-f" name="rating">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </select>
+        </div>
 		<a href="<?=site_url('catalog?genre='.$book['genre']);?>" class="genre"><?=$book['genre_title'];?></a>
 		<?php $this->load->view('guests_interface/html/book-price');?>
 	</div>
