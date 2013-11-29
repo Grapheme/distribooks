82b50b607521bd15bb816a921e79045614156078
KostyaD <thedamaxstudio@gmail.com>
2013-11-29 19:16:24 +0400

    target blank for Terms of Use

--- application/views/guests_interface/includes/auth.php
+++ application/views/guests_interface/includes/auth.php
@@ -22,7 +22,7 @@
 			<div class="div-form-operation">
 				<button type="button" class="auth-button btn-sign-submit btn-loading"><?=lang('signup_button')?></button>
 			</div>
-			<p class="policies"><?=lang('policies_text')?> <a href="<?=site_url('policies')?>"><?=lang('policies_link')?></a></p>
+			<p class="policies"><?=lang('policies_text')?> <a href="<?=site_url('policies')?>" target="_blank"><?=lang('policies_link')?></a></p>
 		</form>
 	</div>
 </div>
