33c9e62142b813923378543619e46ebf52b48fc5
Andrey S <andrey@grapheme.ru>
2013-12-22 05:57:01 +0400

    style updates and copy from the server

--- application/views/guests_interface/index.php
+++ application/views/guests_interface/index.php
@@ -31,7 +31,7 @@
 					<?php $this->load->view('guests_interface/includes/big-nav');?>
 				</div>
 			<?php for($i=0;$i<count($novelty);$i++):?>
-				<div class="grid_1 omega">
+				<div class="grid_1 omega index-box">
 					<?php $this->load->view('guests_interface/html/book-in-shop',array('book'=>$novelty[$i]));?>
 					<?php $this->load->view('guests_interface/html/buyor',array('book_id'=>$novelty[$i]['id'],'mySignedBook'=>$novelty[$i]['signed_book'],'in_basket'=>$novelty[$i]['book_in_basket']));?>
 				</div>
