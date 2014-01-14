111402c69aed4486a0e76dafe1a1cc5431981cf7
vkharseev <vladimir@realitygroup.ru>
2014-01-14 13:38:16 +0400

    Вернул SMTP

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -238,15 +238,15 @@ class MY_Controller extends CI_Controller{
 		
 		$this->load->library('phpmailer');
 		$mail = new PHPMailer();
-		$mail->IsSendmail();
+//		$mail->IsSendmail();
 //		$mail->SMTPDebug = 1;
-	/*	$mail->IsSMTP();
+		$mail->IsSMTP();
 		$mail->SMTPAuth = true;
 		$mail->SMTPSecure = "ssl";
 		$mail->Host = "smtp.yandex.ru";
 		$mail->Port = 465;
 		$mail->Username = "distribbooks@yandex.ru";
-		$mail->Password = "gfd688NNDNS";*/
+		$mail->Password = "gfd688NNDNS";
 		try {
 			$mail->AddReplyTo($from_mail,$from_name);
 			$mail->AddAddress($to);
